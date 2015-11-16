77a6065612fc1c9a76e8d20cfd1010165c0aaa1d
ideyu <gy4ok4ok@gmail.com>
2015-11-16 12:44:03 +0200

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
 
-  resources :articles
+  resources :articles do
+    resource :comments
+    #, only: [:show], path_names: {:show => }
+  end
 
 
 
